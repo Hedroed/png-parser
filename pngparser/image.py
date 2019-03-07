eb0ff4b7bb512ba4caba8c9c1ec3ba80b7c993c7
Hedroed <nathan.rydin@gmail.com>
2019-03-07 19:27:14 +0100

    WIP fix show image pil compatibility

--- pngparser/image.py
+++ pngparser/image.py
@@ -33,6 +33,18 @@ class Image():
         return self.data[pos]
 
     def show(self):
-        pil_img = PilImage.new(self.pixel_type, (self.width, self.height))
-        pil_img.putdata(map(bytes, self.data))
+
+        pil_pixel_type = "RGB"
+        if self.pixel_type == 0:  # Greyscale
+            pil_pixel_type = 'L' # 1
+        elif self.pixel_type == 2:  # RGB
+            pil_pixel_type = 'RGB' # 3
+        elif self.pixel_type == 4:  # Greyscale + alpha
+            pil_pixel_type = 'LA' # 2
+        elif self.pixel_type == 6:  # RGB + Alpha
+            pil_pixel_type = 'RGBA' # 4
+
+
+        pil_img = PilImage.new(pil_pixel_type, (self.width, self.height))
+        pil_img.putdata(list(map(lambda x:x.values, self.data)))
         pil_img.show()
