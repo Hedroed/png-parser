aa91cc67082111dd078b7bbdd3778f526bf428fd
Hedroed <nathan.rydin@gmail.com>
2022-04-20 20:08:08 +0200

    wip test buildkit CI

---
+++ buildkit/go.mod
@@ -0,0 +1,21 @@
+module build
+
+go 1.18
+
+require (
+	github.com/containerd/containerd v1.6.3-0.20220401172941-5ff8fce1fcc6 // indirect
+	github.com/docker/distribution v2.8.0+incompatible // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/moby/buildkit v0.10.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.3-0.20211202183452-c5a74bcca799 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/crypto v0.0.0-20211202192323-5770296d904e // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	google.golang.org/genproto v0.0.0-20211208223120-3a66f561d7aa // indirect
+	google.golang.org/grpc v1.45.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
---
+++ buildkit/image.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/moby/buildkit/client/llb"
+	// "github.com/moby/buildkit/util/system"
+)
+
+func main() {
+
+	inputs := []llb.State{build(), coverage(), pylint()}
+	out := llb.Merge(inputs)
+
+	ret := pythonFS()
+
+	r := ret.With(
+		copyFrom(out, "/", "/app"),
+		llb.Dir("/app"),
+		llb.AddEnv("TEST", "1"),
+	)
+
+	dt, err := r.Marshal(context.TODO(), llb.LinuxAmd64)
+	if err != nil {
+		panic(err)
+	}
+	llb.WriteTo(dt, os.Stdout)
+}
+
+func pythonFS() llb.State {
+	return llb.Image("docker.io/library/alpine:latest").
+		Run(llb.Shlex("apk add --no-cache python3 py3-pip py3-distutils-extra py3-wheel build-base gcc python3-dev zlib-dev jpeg-dev")).
+		Root()
+}
+
+func appFS() (llb.State, llb.State) {
+	py := pythonFS()
+
+	working_dir := llb.Local("working-dir")
+
+	r := copy(working_dir, "/requirements.txt", llb.Scratch(), "/requirements.txt")
+
+	return py.Dir("/app").Run(llb.Shlex("pip install -r requirements.txt"), llb.AddMount("/app", r)).Root(), working_dir
+}
+
+func build() llb.State {
+	app, working_dir := appFS()
+
+	r := app.Run(llb.Shlex("python3 setup.py bdist"), llb.AddMount("/app", working_dir))
+	ret := r.AddMount("/app/dist", llb.Scratch())
+
+	return ret
+}
+
+func coverage() llb.State {
+	app, working_dir := appFS()
+
+	pip := app.Run(llb.Shlex("pip install coverage")).Root()
+
+	r := pip.Dir("/app").AddEnv("COVERAGE_FILE", "/tmp/coverage").Run(llb.Shlex("coverage run -m unittest discover"), llb.AddMount("/app", working_dir, llb.Readonly)).
+		Dir("/app").AddEnv("COVERAGE_FILE", "/tmp/coverage").Run(llb.Shlex("coverage html --directory /out/htmlcov"), llb.AddMount("/app", working_dir, llb.Readonly))
+
+	ret := r.AddMount("/out", llb.Scratch())
+
+	return ret
+}
+
+func pylint() llb.State {
+	app, working_dir := appFS()
+
+	pip := app.Run(llb.Shlex("pip install pylint")).Root()
+
+	r := pip.Dir("/app").Run(llb.Shlex("pylint pngparser -f json --output /app/pylint/report.json"), llb.AddMount("/app", working_dir))
+
+	ret := r.AddMount("/app/pylint", llb.Scratch())
+
+	return ret
+}
+
+// copyFrom has similar semantics as `COPY --from`
+func copyFrom(src llb.State, srcPath, destPath string) llb.StateOption {
+	return func(s llb.State) llb.State {
+		return copy(src, srcPath, s, destPath)
+	}
+}
+
+// copy copies files between 2 states using cp
+func copy(src llb.State, srcPath string, dest llb.State, destPath string) llb.State {
+	return dest.File(llb.Copy(src, srcPath, destPath, &llb.CopyInfo{
+		AllowWildcard:  true,
+		AttemptUnpack:  true,
+		CreateDestPath: true,
+	}))
+}
